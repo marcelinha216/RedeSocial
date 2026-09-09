5ceee0ad7b6efd252bb1a12d0465892bce43e26e
marcelinha216 <marcelapolakoski@gmail.com>
2026-09-09 11:05:27 -0300

    ok

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",()=>(
+    const likeBtn = document.querySelector(".like-btn"):
+    const postMedia =document.querySelector(".post-media");     
+    if(!likeBtn) return;
+
+    const likesConutSpan = likeBtn.querySelector(".likes-count");
+    const bookmarkBtn = document.querySelector(".bookmark-btn");
+
+let isliked = false; 
+let baselikes = 0;
+
+))
